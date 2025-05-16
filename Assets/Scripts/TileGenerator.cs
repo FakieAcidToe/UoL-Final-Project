8fc6a14f9fcb32eac3cbf005aa03adba4b0a8e23
Lukaru <leonidastee1@gmail.com>
2025-05-16 20:21:48 +0800

    Lukarune Tilemap

---
+++ Assets/Scripts/TileGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class TileGenerator
+{
+	public static void GenerateTiles(HashSet<Vector2Int> floorPositions, TilemapVisualizer tilemapVisualizer)
+	{
+		HashSet<Vector2Int> wallPositions = FindWallsInDirections(floorPositions, Direction2D.eightDirectionsList);
+		CreateFloor(tilemapVisualizer, floorPositions);
+		CreateWall(tilemapVisualizer, wallPositions, floorPositions);
+	}
+
+	static void CreateFloor(TilemapVisualizer tilemapVisualizer, HashSet<Vector2Int> floorPositions)
+	{
+		foreach (Vector2Int position in floorPositions)
+		{
+			string neighboursBinaryType = "";
+			foreach (Vector2Int direction in Direction2D.eightDirectionsList)
+			{
+				Vector2Int neighbourPosition = position + direction;
+				neighboursBinaryType += floorPositions.Contains(neighbourPosition) ? "0" : "1";
+			}
+			tilemapVisualizer.PaintSingleFloor(position, neighboursBinaryType);
+		}
+	}
+
+	static void CreateWall(TilemapVisualizer tilemapVisualizer, HashSet<Vector2Int> wallPositions, HashSet<Vector2Int> floorPositions)
+	{
+		foreach (Vector2Int position in wallPositions)
+		{
+			string neighboursBinaryType = "";
+			foreach (Vector2Int direction in Direction2D.eightDirectionsList)
+			{
+				Vector2Int neighbourPosition = position + direction;
+				neighboursBinaryType += floorPositions.Contains(neighbourPosition) ? "1" : "0";
+			}
+			tilemapVisualizer.PaintSingleWall(position, neighboursBinaryType);
+		}
+	}
+
+	static HashSet<Vector2Int> FindWallsInDirections(HashSet<Vector2Int> floorPositions, List<Vector2Int> directionList)
+	{
+		HashSet<Vector2Int> wallPositions = new HashSet<Vector2Int>();
+		foreach (Vector2Int position in floorPositions)
+		{
+			foreach (Vector2Int direction in directionList)
+			{
+				Vector2Int neighbourPosition = position + direction;
+				if (!floorPositions.Contains(neighbourPosition))
+					wallPositions.Add(neighbourPosition);
+			}
+		}
+		return wallPositions;
+	}
+}
---
+++ Assets/Scripts/TilemapVisualizer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapVisualizer : MonoBehaviour
+{
+	[SerializeField] Tilemap floorTilemap, wallTilemap;
+	[SerializeField] TileBase[] floorTiles;
+	[SerializeField] TileBase[] wallTiles;
+
+	//public void PaintFloorTiles(IEnumerable<Vector2Int> floorPositions)
+	//{
+	//	PaintTiles(floorPositions, floorTilemap, floorTile);
+	//}
+	//
+	//void PaintTiles(IEnumerable<Vector2Int> positions, Tilemap tilemap, TileBase tile)
+	//{
+	//	foreach (Vector2Int position in positions)
+	//		PaintSingleTile(tilemap, tile, position);
+	//}
+
+	internal void PaintSingleFloor(Vector2Int position, string binaryType)
+	{
+		int typeAsInt = Convert.ToInt32(binaryType, 2);
+		TileBase tile = null;
+		for (int i = 0; i < WallTypesHelper.wallList.Length; ++i)
+		{
+			HashSet<int> wallSet = WallTypesHelper.wallList[i];
+			if (wallSet.Contains(typeAsInt))
+			{
+				tile = floorTiles[i];
+				break;
+			}
+		}
+
+		if (tile != null)
+			PaintSingleTile(floorTilemap, tile, position);
+	}
+
+	internal void PaintSingleWall(Vector2Int position, string binaryType)
+	{
+		int typeAsInt = Convert.ToInt32(binaryType, 2);
+		TileBase tile = null;
+		for (int i = 0; i < WallTypesHelper.wallList.Length; ++i)
+		{
+			HashSet<int> wallSet = WallTypesHelper.wallList[i];
+			if (wallSet.Contains(typeAsInt))
+			{
+				tile = wallTiles[i];
+				break;
+			}
+		}
+
+		if (tile != null)
+			PaintSingleTile(wallTilemap, tile, position);
+	}
+
+	void PaintSingleTile(Tilemap tilemap, TileBase tile, Vector2Int position)
+	{
+		Vector3Int tilePosition = tilemap.WorldToCell((Vector3Int)position);
+		tilemap.SetTile(tilePosition, tile);
+	}
+
+	public void Clear()
+	{
+		floorTilemap.ClearAllTiles();
+		wallTilemap.ClearAllTiles();
+	}
+}
