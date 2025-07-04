a51e732a9f3e2904f4cdf5ec5a82c14c18031d18
Lukaru <leonidastee1@gmail.com>
2025-07-04 11:35:24 +0800

    Enemy Prefab

--- Assets/Scripts/Game Managers/GameplayManager.cs
+++ Assets/Scripts/Game Managers/GameplayManager.cs
@@ -2,15 +2,25 @@
 
 public class GameplayManager : MonoBehaviour
 {
-	[SerializeField] GameObject playerObj;
+	[Header("Scene References")]
 	[SerializeField] AbstractDungeonGenerator dungeonGenerator;
 	[SerializeField] CameraFollow2D cameraObj;
+
+	[Header("Prefabs")]
+	[SerializeField] GameObject playerPrefab;
+	[SerializeField] GameObject enemyPrefab;
+
+	GameObject playerObj;
 
 	public void GenerateLevel()
 	{
 		dungeonGenerator.GenerateDungeon();
 
-		playerObj.transform.position = (Vector3)dungeonGenerator.GetSpawnLocation();
+		Vector3 spawnLocation = dungeonGenerator.GetSpawnLocation();
+		if (playerObj == null)
+			playerObj = Instantiate(playerPrefab, spawnLocation, Quaternion.identity);
+		else
+			playerObj.transform.position = spawnLocation;
 		cameraObj.target = playerObj.transform;
 		cameraObj.SetPositionToTarget();
 	}
