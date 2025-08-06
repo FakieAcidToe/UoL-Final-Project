0d6bfda77a7e37c11160623c0de0b047892be5db
Lukaru <leonidastee1@gmail.com>
2025-08-06 17:34:02 +0800

    Simple Title Screen

---
+++ Assets/Scripts/Game Managers/TitleManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+	[Header("Scene References")]
+	[SerializeField, Tooltip("Auto set if null")] SceneChanger sceneChanger;
+	[SerializeField] UIFader fadeInScreen;
+
+	[Header("Change Scene Properties")]
+	[SerializeField] int gameplaySceneIndex = 1;
+	[SerializeField] float fadeTime = 0.5f;
+
+	void Awake()
+	{
+		if (sceneChanger == null) sceneChanger = GetComponent<SceneChanger>();
+	}
+
+	public void PlayButton()
+	{
+		StartCoroutine(PlayButtonCoroutine());
+	}
+
+	IEnumerator PlayButtonCoroutine()
+	{
+		if (fadeInScreen.GetCurrentAlpha() < 1f) fadeInScreen.FadeInCoroutine(fadeTime);
+		while (fadeInScreen.GetCurrentAlpha() < 1f)
+			yield return null;
+
+		sceneChanger.LoadSceneByIndex(gameplaySceneIndex);
+	}
+}
---
+++ Assets/Scripts/Utils/SceneChanger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+	public void LoadSceneByName(string sceneName)
+	{
+		SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+	}
+
+	public void LoadSceneByIndex(int sceneIndex)
+	{
+		SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Single);
+	}
+
+	public void ReloadCurrentScene()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log("Quit Game");
+		Application.Quit();
+	}
+}
