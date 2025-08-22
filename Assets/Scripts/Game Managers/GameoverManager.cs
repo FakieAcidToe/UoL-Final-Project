4950e71c3658a2376d0bd9d6efca7b12593d6098
Lukaru <leonidastee1@gmail.com>
2025-08-23 00:14:02 +0800

    Simple Stat Summary Display

--- Assets/Scripts/Game Managers/GameoverManager.cs
+++ Assets/Scripts/Game Managers/GameoverManager.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class GameoverManager : GeneralManager
 {
 	[Header("Scene References")]
 	[SerializeField] Text topText;
+	[SerializeField] StatLine[] statLines;
 
 	[Header("Bestiary List")]
 	[SerializeField] PlayerAnimationSet playerSet;
@@ -19,20 +19,28 @@ public class GameoverManager : GeneralManager
 	[Header("Positioning")]
 	[SerializeField] Vector2 standeePosition;
 	[SerializeField] Vector2 standeeScale = Vector2.one * 2;
+	[SerializeField] float statLineTiming = 0.5f;
 
 	[Header("Strings")]
 	[SerializeField] string topTextWin = "You Win!!";
 	[SerializeField] string topTextLose = "Game Over";
 
+	[Header("Audio")]
+	[SerializeField] AudioClip starSfx;
+
+	float timer = 0;
+	int statIndex = 0;
+
 	protected override void Start()
 	{
 		base.Start();
 
+		// disable statlines
+		foreach (StatLine stat in statLines) stat.gameObject.SetActive(false);
+
 		// testing
 		//SaveManager.Instance.CurrentMiscData.win = true;
 		//SaveManager.Instance.CurrentMiscData.currentPlayCharacter = 4;
-		Debug.Log("Captured: "+SaveManager.Instance.CurrentMiscData.numEnemiesCaptured.ToString());
-		Debug.Log("Killed: "+SaveManager.Instance.CurrentMiscData.numEnemiesKilled.ToString());
 
 		topText.text = SaveManager.Instance.CurrentMiscData.win ? topTextWin : topTextLose;
 
@@ -53,5 +61,21 @@ public class GameoverManager : GeneralManager
 			standee.SetFlipX(Vector2.right * (anims.isFacingRight ? 1 : -1));
 		}
 		standee.transform.localScale = new Vector2(standeeScale.x, standeeScale.y);
+	}
+
+	void Update()
+	{
+		if (statIndex < statLines.Length)
+		{
+			timer += Time.deltaTime;
+			if (timer >= statLineTiming)
+			{
+				statLines[statIndex].gameObject.SetActive(true);
+				PlaySFX(starSfx);
+
+				timer -= statLineTiming;
+				++statIndex;
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/UI/StatLine.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StatLine : MonoBehaviour
+{
+	[SerializeField] Text text;
+	[SerializeField] StatValue statValue = StatValue.numEnemiesCaptured;
+
+	enum StatValue
+	{
+		numEnemiesCaptured,
+		numEnemiesKilled
+	}
+
+	void Start()
+	{
+		if (text != null)
+			text.text = GetStatValueString();
+	}
+
+	string GetStatValueString()
+	{
+		switch (statValue)
+		{
+			default:
+				return "";
+			case StatValue.numEnemiesCaptured:
+				return "Captured " + (SaveManager.Instance == null ? "0" : SaveManager.Instance.CurrentMiscData.numEnemiesCaptured.ToString()) + " Monsters";
+			case StatValue.numEnemiesKilled:
+				return "Defeated " + (SaveManager.Instance == null ? "0" : SaveManager.Instance.CurrentMiscData.numEnemiesKilled.ToString()) + " Monsters";
+		}
+	}
+
+	void OnValidate()
+	{
+		Start();
+	}
+}
